f043b9818513c1290f723a35f8ce2a90a630a698
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-06-09 21:28:06 -0300

    Update discreta.py

--- discreta.py
+++ discreta.py
@@ -1,32 +1,107 @@
-salas = int(input())
-pessoas = int(input())
-qnt = 0
+import matplotlib.pyplot as plt
 
-for i in range(pessoas + 1):
+def fact(fator):
+    fatorial = 1
+    for j in range(1, fator+1):
+        fatorial *= j
 
-    nFact = 1
-    for n in range(1, pessoas + 1):
-        nFact *= n
+    return int(fatorial)
 
-    kFact = 1
-    for k in range(1, qnt + 1):
-        kFact *= k
 
-    mFact = 1
-    for m in range(1, pessoas - qnt + 1):
-        mFact *= m
+def pessoas(salas, pessoas):
+    saida = [[], []]
+    qnt = 0
 
-    if qnt == 0 or qnt == 10:
-        comb = 1
-    else:
-        comb = nFact / (kFact * mFact)
+    for i in range(pessoas + 1):
 
-    resto = (salas - 1) ** (pessoas - qnt)
+        pessoasfact = fact(pessoas)
 
-    divisor = salas ** pessoas
+        qntfact = fact(qnt)
 
-    probabilidade = (comb * resto) / divisor
+        m = fact(pessoas - qnt)
 
-    print(f"O número de maneiras de distribuir na sala é de {divisor} maneiras e a probabilidade de ter {qnt} pessoas na sala é de {comb}/{divisor} = {probabilidade}")
-    qnt += 1
+        if qnt == 0 or qnt == 10:
+            comb = 1
+        else:
+            comb = pessoasfact / (qntfact * m)
 
+        resto = (salas - 1) ** (pessoas - qnt)
+
+        divisor = salas ** pessoas
+
+        probabilidade = (comb * resto) / divisor
+
+        print(f"O número de maneiras de distribuir na sala é de {divisor} maneiras e a probabilidade de ter {qnt} pessoas na sala é de {comb}/{divisor} = {probabilidade}")
+        qnt += 1
+        saida[0].append(qnt)
+        saida[1].append(probabilidade)
+
+    return saida
+
+
+def quantas(qnt, ligacoes):
+    temp = 0
+    saida = [[], []]
+
+    for i in range(qnt + 1):
+        quantasFact = fact(qnt)
+        tempFact = fact(temp)
+        ligFact = fact(ligacoes)
+
+        prob = (quantasFact**4)/(ligFact*((tempFact*fact(qnt-temp))**2))
+
+        print(f"A probabildade do sólido A ter {temp} de temperatura com {ligacoes} ligações é de {prob:.5f}%")
+        temp += 1
+        saida[0].append(temp)
+        saida[1].append(prob)
+
+    return saida
+
+
+def show_quantas(quantasn, ligacoesn):
+    dados = quantas(quantasn, ligacoesn)
+
+    eixox = dados[0]
+    eixoy = dados[1]
+
+    ticks = [j for j in range(len(eixox)+1)]
+
+    labels = [""]
+
+    for i in range(len(eixox)):
+        labels.append(i)
+
+    fig, ax = plt.subplots(figsize=(8.5, 6))
+    bar_container = ax.bar(eixox, eixoy, color='purple')
+    ax.set_xticks(ticks, labels)
+    ax.set(ylabel="Probabilidade (%)", title="Distribução de quantas", xlabel="Temperatura", ylim=(0, 0.4))
+    ax.bar_label(bar_container, fmt="{:,.5f}%")
+
+    plt.show()
+
+
+def show_pessoas(salasn, pessoasn):
+    dados = pessoas(salasn, pessoasn)
+
+    eixox = dados[0]
+    eixoy = dados[1]
+
+    ticks = [j for j in range(len(eixox) + 1)]
+
+    labels = [""]
+
+    for i in range(len(eixox)):
+        labels.append(i)
+
+    fig, ax = plt.subplots(figsize=(9, 6))
+    bar_container = ax.bar(eixox, eixoy, color='red')
+    ax.set_xticks(ticks, labels)
+    ax.set(ylabel="Probabilidade (%)", title="Distribuição das pessoas", xlabel="Pessoas na sala")
+    ax.bar_label(bar_container, fmt="{:,.5f}%")
+
+    plt.show()
+
+
+show_quantas(8, 16)
+
+show_pessoas(2, 10)
